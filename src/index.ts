e1c2fd42b314bf87a48137709697bc6f6f81b2bd
Alexis Hope <alexis@cvofhope.com>
2021-10-08 12:30:17 +1100

    feat: implement an id generator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 
-function simpleId(): string {
-  return "abc";
+const byteHexTable = () => {
+  const table = new Array(256).fill(0)
+  return table.map((_, i) => (i + 0x100).toString(16).substring(1))
 }
 
-export default simpleId;
+const singleton = (fn, ...args) => {
+  let tmp
+  return () => {
+    if (tmp) {
+      return tmp
+    }
+    tmp = fn.apply(...args)
+    return tmp
+  }
+}
+
+const getByteHexTable = singleton(byteHexTable)
+
+const whatwgRNG = (length = 16) => {
+  const rnds8 = new Uint8Array(length)
+  crypto.getRandomValues(rnds8)
+  return rnds8
+}
+
+const plainId = (len = 16): string => {
+  const table = getByteHexTable()
+  return whatwgRNG(len)
+    .reduce((str, n) => str + table[n], '')
+    .slice(0, len)
+}
+
+export default plainId
